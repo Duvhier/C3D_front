9386586b7e31619f6679d28b7943e5b07494379a
Duvier <duvier.tavera01@unicatolica.edu.co>
2025-05-31 01:47:34 -0500

    Fix bug

--- src/services/bookService.js
+++ src/services/bookService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = import.meta.env.VITE_API_URL || 'https://c3-d-back-nkt5.vercel.app';
+const API_URL = import.meta.env.VITE_API_URL || 'https://c3-d-back-nkt5.onrender.com';
 
 const api = axios.create({
   baseURL: API_URL,
--- src/services/genreService.js
+++ src/services/genreService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = import.meta.env.VITE_API_URL || 'https://c3-d-back-nkt5.vercel.app';
+const API_URL = import.meta.env.VITE_API_URL || 'https://c3-d-back-nkt5.onrender.com';
 
 export const getGenres = async () => {
 
